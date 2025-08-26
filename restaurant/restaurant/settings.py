8b9422b1036cea1df87bdb98abe41b0fa8928328
Soheil <soheil.ce.99@gmail.com>
2025-08-26 11:41:15 +0330

    allowed Hosts-2

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -130,10 +135,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
 
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # ckeditor
 CKEDITOR_CONFIGS = {
